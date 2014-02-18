4c45e13c1fdae324335ecfa9514fbfb65fb8a33e
erooijak <erooijakkers@sogyo.nl>
2014-02-18 22:45:35 +0100

    six with description

--- six.java
+++ six.java
@@ -1,4 +1,20 @@
 public class six {
+
+	/*
+	*
+	* The sum of the squares of the first ten natural numbers is,
+	* 12 + 22 + ... + 102 = 385
+	*
+	* The square of the sum of the first ten natural numbers is,
+	* (1 + 2 + ... + 10)2 = 552 = 3025
+	*
+	* Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+	* 
+	* Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+	*
+	* SOLUTION: BRUTE FORCE
+	*
+	*/
 	public static void main (String[] args) {
 		
 		int answer = AddAndSquare(100) - SquareAndAdd(100);
