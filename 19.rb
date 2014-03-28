ab81a0dfa6c3d1f783909a6125a89973bace1472
unknown <erwinrooijakkers@gmail.com>
2014-03-29 00:18:08 +0100

    19

--- 19.rb
+++ 19.rb
@@ -19,7 +19,6 @@ leap_beginnings = beginnings.map.with_index {|n,i| (i > 1) ? n+1 : n }
 
 # Then: collect all Sundays. First the amount of days in the 100 years is calculated and every number that is divisible by seven is a Sunday
 sundays = Array.new
-number_of_days = 0
 number_of_days = (0..100).reduce(0) { |tot,i| tot + ((i%4 == 0) ? 366 : 365) } 
 
 for i in 365..number_of_days #start after 1 non-leap year (365 days)
@@ -29,7 +28,6 @@ for i in 365..number_of_days #start after 1 non-leap year (365 days)
 end
 
 # Then collect numbers of beginning of month for all years by adding the arrays to each other.
-
 beginnings = [0,31,59,90,120,151,181,212,243,273,304,334] # jan = 31, feb =jan + 28, mar = jan + feb + 31, etc.
 leap_beginnings = beginnings.map.with_index {|n,i| (i > 1) ? n+1 : n }
 
@@ -53,4 +51,5 @@ starting_days_of_month = []
   starting_days_of_month += days.map { |day| day + offset(starting_days_of_month) }
 end
 
+#Then intersect the 'starting days of month'-numbers and the sunday-numbers and count.
 puts (starting_days_of_month & sundays).count
