085714cb0ed5236f2de9b2663b607fc798bab313
unknown <erwinrooijakkers@gmail.com>
2014-04-12 15:10:26 +0200

    1 - 20ms

--- 1.rb
+++ 1.rb
@@ -1,12 +1,7 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-start = Time.now
-i = 0
-sum = 0
-for i in 1..1000
-	if i % 3 == 0 || i % 5 == 0
-		sum += i
-	end
-end
 
-puts "The answer is #{sum} and it took #{ (Time.now - start) * 1000} milliseconds."
+start = Time.now
+answer = 1.upto(1000).select{|n| n%3==0||n%5==0}.inject(:+)
+
+puts "The answer is #{answer} and it took #{ (Time.now - start) * 1000} milliseconds."
