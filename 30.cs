4ade40d6d3f46ac1d307b8156a25c4b01b16629f
unknown <erwinrooijakkers@gmail.com>
2014-04-19 23:57:12 +0200

    30 - 1s - inspiration

---
+++ 30.cs
@@ -0,0 +1,34 @@
+// Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+//
+//     1634 = 14 + 64 + 34 + 44
+//     8208 = 84 + 24 + 04 + 84
+//     9474 = 94 + 44 + 74 + 44
+//
+// As 1 = 14 is not a sum it is not included.
+//
+// The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+//
+// Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+// Oneliner in C# - Solution taken from forum for inspiration.
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var answer = Enumerable.Range(2, 500000).Where(t => t == t.ToString().ToArray().Select(digit => int.Parse(digit.ToString())).Aggregate(0, (acc, num) => acc + (int)Math.Pow(num, 5))).Sum();
+            sw.Stop();
+            var elapsed = sw.ElapsedMilliseconds;
+
+            Console.WriteLine("The answer is {0} and it took {1} milliseconds.", answer, elapsed);
+        }
+    }
+}
