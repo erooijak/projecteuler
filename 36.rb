db5bd02796f76c663841d7759a449164231b7214
unknown <erwinrooijakkers@gmail.com>
2014-04-21 21:35:18 +0200

    36 - 0.9s

---
+++ 36.rb
@@ -0,0 +1,18 @@
+# The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+#
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+#
+# (Please note that the palindromic number, in either base, may not include leading zeros.)
+
+def palindromic?(n)
+  n.to_s == n.to_s.reverse
+end
+
+def find_sum_of_palindromic_numbers_in_base2_and_base10_upto(n)
+  (1..n).reduce {|acc,val| palindromic?(val) && palindromic?(val.to_s(2)) ? acc + val : acc}
+end
+
+start = Time.now
+answer = find_sum_of_palindromic_numbers_in_base2_and_base10_upto(1_000_000)
+
+puts "The answer is #{answer} and it took #{Time.now-start} seconds."
