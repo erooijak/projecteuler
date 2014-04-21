aea3439378914cb4500c7e32b8fc95dc9aab5783
unknown <erwinrooijakkers@gmail.com>
2014-04-21 21:38:42 +0200

    36 - 0.9s

--- 36.rb
+++ 36.rb
@@ -8,11 +8,11 @@ def palindromic?(n)
   n.to_s == n.to_s.reverse
 end
 
-def find_sum_of_palindromic_numbers_in_base2_and_base10_upto(n)
+def sum_of_palindromic_numbers_in_base2_and_base10_upto(n)
   (1..n).reduce {|acc,val| palindromic?(val) && palindromic?(val.to_s(2)) ? acc + val : acc}
 end
 
 start = Time.now
-answer = find_sum_of_palindromic_numbers_in_base2_and_base10_upto(1_000_000)
+answer = sum_of_palindromic_numbers_in_base2_and_base10_upto(1_000_000)
 
 puts "The answer is #{answer} and it took #{Time.now-start} seconds."
