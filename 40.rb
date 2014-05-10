226081c29996058645ac8bbb13dbadd8fb85069e
unknown <erwinrooijakkers@gmail.com>
2014-05-10 20:16:39 +0200

    40 - 1s

--- 40.rb
+++ 40.rb
@@ -9,6 +9,6 @@
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 
 start = Time.now
-answer = ("0".."200000").reduce {|acc, n| acc.concat(n)}.scan(/\d/).map{|d| d.to_i }.values_at(10**0,10**1,10**2,10**3,10**4,10**5,10**6).inject(:*)
+answer = [*0..200000].join.scan(/\d/).map{|d| d.to_i }.values_at(10**0,10**1,10**2,10**3,10**4,10**5,10**6).inject(:*)
 
-puts "The answer is #{answer} and it took #{Time.now - start} seconds."
+puts "The answer is #{answer} and it took #{Time.now - start} seconds."		
