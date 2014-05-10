ee11c853412186c83985512993eb7b16bf36becc
unknown <erwinrooijakkers@gmail.com>
2014-05-10 13:36:31 +0200

    36 - 0.9s

--- 36.rb
+++ 36.rb
@@ -4,15 +4,4 @@
 #
 # (Please note that the palindromic number, in either base, may not include leading zeros.)
 
-def palindromic?(n)
-  n.to_s == n.to_s.reverse
-end
-
-def sum_of_palindromic_numbers_in_base2_and_base10_upto(n)
-  (1..n).reduce {|acc,val| palindromic?(val) && palindromic?(val.to_s(2)) ? acc + val : acc}
-end
-
-start = Time.now
-answer = sum_of_palindromic_numbers_in_base2_and_base10_upto(1_000_000)
-
-puts "The answer is #{answer} and it took #{Time.now-start} seconds."
+puts (1..1_000_000).reduce {|acc,val| val.to_s == val.to_s.reverse && val.to_s(2) == val.to_s(2).reverse ? acc + val : acc}
