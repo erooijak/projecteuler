aedd3b982579efb242ac2215cfc20c0dd1a5ddaf
erooijak <erooijakkers@sogyo.nl>
2014-05-25 17:23:10 +0200

    48 - 0.2s

--- 48.rb
+++ 48.rb
@@ -2,4 +2,4 @@
 
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
-puts (1..1000).map { |n| n**n }.inject(:+).to_s[-10,10]
+puts (1..1000).map {|n| n**n}.inject(:+).to_s[-10,10]
