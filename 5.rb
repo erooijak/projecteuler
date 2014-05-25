2591f1d4c161c01be345269277ea4814cb280aa5
erooijak <erooijakkers@sogyo.nl>
2014-05-25 16:35:39 +0200

    5 - 0.03ms

---
+++ 5.rb
@@ -0,0 +1,8 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+start = Time.now
+answer = (1..20).inject(:lcm)
+
+puts "The answer is #{answer} and it took #{(Time.now-start)*1000} milliseconds."
