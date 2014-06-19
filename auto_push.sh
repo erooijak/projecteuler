852c89e8911ccfabb150110740a5d9cd90955e7d
erooijak <erooijakkers@sogyo.nl>
2014-06-19 15:27:34 +0200

    autocommit on change

---
+++ auto_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# gitwait - watch file and git commit all changes as they happen
+#
+
+while true; do
+  inotifywait -qq -e CLOSE_WRITE lychrel_quest.txt
+  git add . ; git commit -a -m 'autocommit on change'; git push
+done
