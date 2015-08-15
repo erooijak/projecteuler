eca4e0134d6ee234525695a9a1506bdb45686854
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-08-15 20:08:24 +0200

    60 - try for 4 primes, need to adjust to higher values for concatenations for 5

--- 60.scm
+++ 60.scm
@@ -23,7 +23,7 @@
   (let loop ((p 3)          ; Maintains invariant p = 2j + 1
              (q 4)          ; Maintains invariant q = 2j + 2jj
              (j 1)
-             (result '(2))) ; Start with 2, is missed otherwise.
+             (result '(2))) ; Start with 2, is missed otherwise
     (define (lp result)
       (loop (+ p 2)
             (+ q p p 2)
@@ -31,9 +31,9 @@
             result))
     (define (eradicate!)
       (do ((q q (+ q p)))
-          ((> q n))
+        ((> q n))
         (vector-set! vec q #f)))
-
+    
     (cond ((> j n) (reverse result))
           ((vector-ref vec j) (eradicate!)
                               (lp (cons p result)))
@@ -48,7 +48,8 @@
 ; Create a hash table with key a number and value if it is prime or not
 ; for fast lookup in prime? method.
 (define (make-primes-table input-primes)
-  (let loop ((lst input-primes) (acc (make-vector max-index-to-check #f)))
+  (let loop ((lst input-primes) 
+             (acc (make-vector max-index-to-check #f)))
     (if (empty? lst)
         acc
         (begin 
@@ -70,7 +71,8 @@
 
 ; www.stackoverflow.com/questions/12834562/scheme-number-to-list#12841962
 (define (number->list n)
-  (let loop ((n n) (acc '()))
+  (let loop ((n n) 
+             (acc '()))
     (if (< n 10)
         (cons n acc)
         (loop (quotient n 10)
@@ -78,7 +80,8 @@
 
 ; www.stackoverflow.com/questions/1683479/how-to-convert-a-list-to-num-in-scheme#1688960
 (define (list->number lst)
-  (let loop ((n 0) (lst lst))
+  (let loop ((n 0) 
+             (lst lst))
     (if (empty? lst)
         n
         (loop (+ (* 10 n) (car lst)) (cdr lst)))))
@@ -88,10 +91,9 @@
 
 (define (get-prime-pairs lst)
   (define (find-all-prime-pairs prime lst)
-    (let loop 
-      ((prime prime) 
-       (lst lst) 
-       (acc '()))
+    (let loop ((prime prime) 
+               (lst lst) 
+               (acc '()))
       (if (null? lst) acc
           (if (prime? (list->number (append prime (car lst))))
               (loop prime (cdr lst) (cons (list prime (car lst)) acc))
@@ -100,10 +102,10 @@
 
 (define allowed-pairs (get-prime-pairs primes-list-split))
 
-allowed-pairs
+
 ;; Use list of pairs for efficient search for groups of five that satisfy constraints
 
-; Create list of unique first items in the pairs 
+; Create list of unique first items in the pairs.
 (define (get-first-elements lst)
   (if (null? lst) '()
       (cons (caar lst) (get-first-elements (cdr lst)))))
@@ -143,22 +145,48 @@ allowed-pairs
    list-of-unique-first-primes allowed-pairs))
 
 ; Exclude primes with less than four potential partners.
-
-; Note: now checking for THREE! To see if we can reproduce example with four primes.
 (define (get-primes-with-more-than-four-partners hash-lst)
   (get-first-elements (filter (lambda (x) (> (length x) 3)) hash-lst)))
 
 (define primes-with-more-than-four-partners 
   (get-primes-with-more-than-four-partners hash-keyed-by-starting-prime))
 
-; From primes create sets of which constraints are met;
+; From primes create sets of which constraints are met.
 
+; Try adding new values for each prime in the reduced list of primes with four partners.
+; If the prime can be concatenated to the front and back of every prime in the set so far
+; (i.e., the resulting pairs are both in the list of pairs) we add the prime and continue 
+; to try add a next prime, if this is not possible we discard the previous prime and 
+; try the next prime of the list. This stops once we went through all starting primes.
+; The result is a list of all combinations that satisfy the constraints.
+; Note: this algorithm misses the combinations that are not from lowest to highest if we
+;       do not continue the search after having found one.
+
+; Takes as input a prime in list form, a list of those primes and a list of prime pairs.
 (define (can-be-added? prime set lst-of-pairs)
   (cond ((null? set) #t)
         ((and (contains? lst-of-pairs (list (car set) prime))
               (contains? lst-of-pairs (list prime (car set))))
          (can-be-added? prime (cdr set) lst-of-pairs))
         (else #f)))
+
+(define (get-all-combinations-that-satisfy-constraints primes lst-of-pairs)
+  (define (get-combinations-for-start-prime start-prime)
+    (let loop ((primes primes)
+               (acc '()))
+      (cond ((null? primes) acc)
+            ((can-be-added? (car primes) acc lst-of-pairs)
+             (loop (cdr primes) (append (car primes) acc)))
+            (else (loop (cdr primes) acc)))))
+  
+  (let loop ((primes primes)
+             (result '()))
+    (cond ((null? primes) result)
+          (= 4 (length (get-combinations-for-start-prime (car primes)))
+           (loop (cdr primes) (append (get-combinations-for-start-prime (car primes)) result)))
+          (else (loop (cdr primes) result)))))
+
+(get-all-combinations-that-satisfy-constraints primes-with-more-than-four-partners allowed-pairs)
 
 ; Sum them
 (define (sum lst)
